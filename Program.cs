461fbbbd60b77d7058ed4b3768bbd4f008b72a3b
ZussiMane <igoryunja@gmail.com>
2024-03-26 11:19:09 +0300

    Добавил решение третьей задачи

--- Program.cs
+++ Program.cs
@@ -71,3 +71,34 @@
 // Задача 3: Напишите программу, которая перевернёт одномерный массив 
 // (первый элемент станет последним, второй – предпоследним и т.д.)
 
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 3, 5, 6, 7, 8 }; // Исходный массив
+        int temp; // Временная переменная для обмена значениями
+
+        // Вывод исходного массива
+        Console.Write("Исходный массив: ");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " "); 
+        }
+
+        // Реверсирование массива
+        for (int i = 0; i < numbers.Length / 2; i++)
+        {
+            temp = numbers[i];
+            numbers[i] = numbers[numbers.Length - 1 - i];
+            numbers[numbers.Length - 1 - i] = temp;
+        }
+
+        // Вывод измененного массива
+        Console.Write("Перевернутый массив: ");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+}
